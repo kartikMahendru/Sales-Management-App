e66a38ccec1a1138ba5df9fabdf56db026ed13c6
kartik mahendru <mahendru.kartik@gmail.com>
2018-09-01 19:37:24 +0530

    Sign Up database/storage created

--- app/src/main/java/project/avishkar/salesmanagement/MainActivity.java
+++ app/src/main/java/project/avishkar/salesmanagement/MainActivity.java
@@ -10,19 +10,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView retailer,salesperson;
+    private TextView manager,salesperson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        retailer=findViewById(R.id.retailer);
+        manager = findViewById(R.id.Manager);
         salesperson=findViewById(R.id.salesperson);
 
         // setting on click for sign up for retailer
-        retailer.setOnClickListener(new View.OnClickListener() {
+        manager.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUpRetailer.class);
+                Intent intent = new Intent(MainActivity.this, SignUpSalesManager.class);
                 startActivity(intent);
                 finish();
             }
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // for underline purpose(UI)
         SpannableString text = new SpannableString("Sign up as a Retailer");
         text.setSpan(new UnderlineSpan(), 0, 21, 0);
-        retailer.setText(text);
+        manager.setText(text);
 
         // for underline purpose(UI)
         SpannableString text1 = new SpannableString("Sign up as a Salesperson");
