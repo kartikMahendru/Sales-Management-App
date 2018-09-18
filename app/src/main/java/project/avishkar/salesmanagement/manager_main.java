d997027ccfb1febebecd587e40cbd544abb4debe
utkarsh22garg <utkarsh22garg@yahoo.com>
2018-09-18 23:29:51 +0530

    Created the swipeleft lift view for the inventory

--- app/src/main/java/project/avishkar/salesmanagement/manager_main.java
+++ app/src/main/java/project/avishkar/salesmanagement/manager_main.java
@@ -1,6 +1,8 @@
 package project.avishkar.salesmanagement;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,10 +15,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.BounceInterpolator;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.ArrayList;
 
 public class manager_main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    private SwipeMenuListView listView;
+    private ArrayList<InventoryItem> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +51,56 @@ public class manager_main extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /********Working for the list view to show the items on the dashboard*********/
+
+        listView=findViewById(R.id.items_list);
+
+        SwipeMenuCreator creator= new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+
+                // create "open" item
+
+                SwipeMenuItem openItem = new SwipeMenuItem(getApplicationContext());
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9, 0xCE)));
+                openItem.setWidth(dp2px(90));
+                openItem.setTitle("Open");
+                openItem.setTitleSize(18);
+                openItem.setTitleColor(Color.WHITE);
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+
+                SwipeMenuItem deleteItem=new SwipeMenuItem(getApplicationContext());
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xC9,0xC9,0xCE)));
+                deleteItem.setWidth(dp2px(90));
+                deleteItem.setTitle("Delete");
+                deleteItem.setTitleSize(18);
+                deleteItem.setTitleColor(Color.RED);
+                menu.addMenuItem(deleteItem);
+            }
+
+            public int dp2px(int dp)
+            {
+                return (int) (dp * getApplicationContext().getResources().getDisplayMetrics().density + 0.5f);
+            }
+        };
+
+        listView.setMenuCreator(creator);
+        // for creating a bounce effect
+        listView.setCloseInterpolator(new BounceInterpolator());
+
+        ArrayList<InventoryItem> data = new ArrayList<>();
+        data.add(new InventoryItem("Motorola",1000));
+        data.add(new InventoryItem("Charger",300));
+        data.add(new InventoryItem("Headphones",200));
+        data.add(new InventoryItem("MacBook Air",500));
+
+        CustomAdapter mAdapter = new CustomAdapter(getApplicationContext(),data);
+        listView.setAdapter(mAdapter);
     }
 
     @Override
@@ -81,6 +141,8 @@ public class manager_main extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        //don't finish activites here
+
         if (id == R.id.my_account) {
             //show the manager's account
             Intent intent = new Intent(manager_main.this,AccountManager.class);
@@ -91,8 +153,17 @@ public class manager_main extends AppCompatActivity
         } else if (id == R.id.statistics) {
 
         } else if (id == R.id.nav_share) {
-
+            // share the app link with others
         } else if (id == R.id.nav_send) {
+            // share the invitation link
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            String inviteCode="invite code"; // set the invite code of the manager
+            sendIntent.putExtra(Intent.EXTRA_TEXT,inviteCode);
+            sendIntent.setType("text/plain");
+            if (sendIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(sendIntent);
+            }
 
         }
 
