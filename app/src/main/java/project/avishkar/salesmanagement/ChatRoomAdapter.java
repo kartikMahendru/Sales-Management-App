b38e964084c08eb429b8c18865d4d7a60895c208
kartik mahendru <mahendru.kartik@gmail.com>
2018-09-26 15:59:10 +0530

    global chatroom

---
+++ app/src/main/java/project/avishkar/salesmanagement/ChatRoomAdapter.java
@@ -0,0 +1,94 @@
+package project.avishkar.salesmanagement;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by user on 9/26/18.
+ */
+
+public class ChatRoomAdapter extends RecyclerView.Adapter {
+
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
+
+
+    private Context mContext;
+    private ArrayList<BaseMessage> mMessageList;
+
+    public ChatRoomAdapter(Context context, ArrayList<BaseMessage> messageList) {
+        mContext = context;
+        mMessageList = messageList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessageList.size();
+    }
+
+    // Determines the appropriate ViewType according to the sender of the message.
+    @Override
+    public int getItemViewType(int position) {
+        final BaseMessage message = (BaseMessage) mMessageList.get(position);
+        // here we will extract name from id in shared preference and then decide if current message was sent or received
+        SessionManager sm = new SessionManager(mContext);
+        HashMap<String, String> mp = sm.getUserDetails();
+        String id = mp.get("id");
+        String role = mp.get("role");
+
+        if(message.getMsgRole().equals(id))
+            return VIEW_TYPE_MESSAGE_SENT;
+        else
+            return VIEW_TYPE_MESSAGE_RECEIVED;
+    }
+
+    // Inflates the appropriate layout according to the ViewType.
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+
+        if (viewType == VIEW_TYPE_MESSAGE_SENT) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.chat_room_sent, parent, false);
+            return new MessageHolder(view);
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.chat_room_received, parent, false);
+            return new MessageHolder(view);
+        }
+
+        return null;
+    }
+
+    // Passes the message object to a ViewHolder so that the contents can be bound to UI.
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        BaseMessage message = (BaseMessage) mMessageList.get(position);
+
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_MESSAGE_SENT:
+                ((ReceivedMessageHolder) holder).bind(message);
+                break;
+            case VIEW_TYPE_MESSAGE_RECEIVED:
+                ((ReceivedMessageHolder) holder).bind(message);
+        }
+
+
+    }
+}
+
+
